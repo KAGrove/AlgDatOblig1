e5e02f5fcd1f1c685e89b9f463a91d00baf0c2a5
KAGrove <89571237+KAGrove@users.noreply.github.com>
2023-02-18 17:59:22 +0100

    Oppgave 1 og 2 bestått

---
+++ src/main/java/no/oslomet/cs/algdat/Oblig1/Oblig1.java
@@ -0,0 +1,107 @@
+package no.oslomet.cs.algdat.Oblig1;
+
+////// Løsningsforslag Oblig 1 ////////////////////////
+
+import java.lang.UnsupportedOperationException;
+import java.util.NoSuchElementException;
+
+public class Oblig1 {
+    private Oblig1() {}
+
+    ///// Oppgave 1 //////////////////////////////////////
+    public static int maks(int[] a) {
+        if(a.length == 0){
+            throw new NoSuchElementException("Listen er tom!");
+        }
+        for(int i = 0; i < a.length-1; i++)
+            if(a[i] > a[i+1]){
+                int tmp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = tmp;
+            }
+        return a[a.length-1];
+    }
+
+    public static int ombyttinger(int[] a) {
+        if(a.length == 0){
+            throw new NoSuchElementException("Listen er tom!");
+        }
+        int antOmbyttinger = 0;
+        for(int i = 0; i < a.length-1; i++)
+            if(a[i] > a[i+1]){
+                int tmp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = tmp;
+                antOmbyttinger++;
+            }
+        return antOmbyttinger;
+    }
+
+    ///// Oppgave 2 //////////////////////////////////////
+    public static int antallUlikeSortert(int[] a) {
+        if (a.length == 0){
+            return 0;
+        }
+        int ulike = 1;
+        for(int i = 1; i < a.length; i++){
+            if(a[i] < a[i-1]){
+                throw new IllegalStateException();
+            }
+            if(a[i] > a[i-1]){
+                ulike++;
+            }
+        }
+        return ulike;
+    }
+
+    ///// Oppgave 3 //////////////////////////////////////
+    public static int antallUlikeUsortert(int[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 4 //////////////////////////////////////
+    public static void delsortering(int[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 5 //////////////////////////////////////
+    public static void rotasjon(char[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 6 //////////////////////////////////////
+    public static void rotasjon(char[] a, int k) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 7 //////////////////////////////////////
+    /// 7a)
+    public static String flett(String s, String t) {
+        throw new UnsupportedOperationException();
+    }
+
+    /// 7b)
+    public static String flett(String... s) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 8 //////////////////////////////////////
+    public static int[] indekssortering(int[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 9 //////////////////////////////////////
+    public static int[] tredjeMin(int[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 10 //////////////////////////////////////
+    public static int bokstavNr(char bokstav) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean inneholdt(String a, String b) {
+        throw new UnsupportedOperationException();
+    }
+
+}  // Oblig1
